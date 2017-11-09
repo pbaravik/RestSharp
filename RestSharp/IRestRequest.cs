6105263ed5023de48f37b9cf6d5db50ac3315537
Baravik, Pavel <pavel.baravik@ihsmarkit.com>
2017-11-09 11:03:32 +0300

    update copyright notice

--- RestSharp/IRestRequest.cs
+++ RestSharp/IRestRequest.cs
@@ -13,6 +13,9 @@
 //   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 //   See the License for the specific language governing permissions and
 //   limitations under the License. 
+// 
+//   IHS Markit has made changes to the open source software, RestSharp, which is licensed under Apache 2.0. 
+//   Details about the nature of the changes can be found here: https://github.com/pbaravik/RestSharp/commit/83e8751cab8f5291dc1c68b00a59259b2095ce8f
 
 #endregion
 
